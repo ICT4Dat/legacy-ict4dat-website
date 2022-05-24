877d4b421cd63cee66bcfc52805ef3ccb8e1f24e
Paul Spiesberger <paul@spiesberger.eu>
2022-05-24 15:22:40 +0200

    Remove Xing and update Mastodon Links

--- header_social.php
+++ header_social.php
@@ -5,10 +5,6 @@
         
         <a href="http://www.new.facebook.com/home.php#/group.php?gid=31033330019" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/social/facebook_grey.png" align="absmiddle" alt="facebook" name="facebook" onmouseover="facebook.src='<?php bloginfo('template_directory'); ?>/images/social/facebook_color.png';" onMouseOut="facebook.src='<?php bloginfo('template_directory'); ?>/images/social/facebook_grey.png';"/></a>
         
-        <a href="https://www.xing.com/net/ict4ddotat/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/social/xing_grey.png" align="absmiddle" alt="xing" name="xing" onmouseover="xing.src='<?php bloginfo('template_directory'); ?>/images/social/xing_color.png';" onMouseOut="xing.src='<?php bloginfo('template_directory'); ?>/images/social/xing_grey.png';"/></a>
-        
-        
-        
         <a href="http://twitter.com/ict4ddotat" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/social/twitter_grey.png" align="absmiddle" alt="twitter" name="twitter" onmouseover="twitter.src='<?php bloginfo('template_directory'); ?>/images/social/twitter_color.png';" onMouseOut="twitter.src='<?php bloginfo('template_directory'); ?>/images/social/twitter_grey.png';" /></a>
        
          <a href="http://www.linkedin.com/groups?gid=149868" target="_blank" ><img id="social_linkedin" src="<?php bloginfo('template_directory'); ?>/images/social/in_grey.png" align="absmiddle" alt="linkedin" name="linkedin" onmouseover="linkedin.src='<?php bloginfo('template_directory'); ?>/images/social/in_color.png';" onMouseOut="linkedin.src='<?php bloginfo('template_directory'); ?>/images/social/in_grey.png';" /></a>
@@ -17,7 +13,8 @@
 
 		<a href="http://flickr.com/photos/ict4d/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/social/flickr_grey.png" align="absmiddle" alt="flickr" name="flickr" onmouseover="flickr.src='<?php bloginfo('template_directory'); ?>/images/social/flickr_color.png';" onMouseOut="flickr.src='<?php bloginfo('template_directory'); ?>/images/social/flickr_grey.png';" /></a>
 
-        <a rel="me" href="https://mastodon.social/@spipau" target="_blank"><img height="13px" width="12px" src="<?php bloginfo('template_directory'); ?>/images/social/mastodon_grey.svg" align="absmiddle" alt="mastodon" name="mastodon" onmouseover="mastodon.src='<?php bloginfo('template_directory'); ?>/images/social/mastodon.svg';" onMouseOut="mastodon.src='<?php bloginfo('template_directory'); ?>/images/social/mastodon_grey.svg';" /></a>
+        <a rel="me" href="https://digitalcourage.social/@ict4ddotat" target="_blank"><img height="13px" width="12px" src="<?php bloginfo('template_directory'); ?>/images/social/mastodon_grey.svg" align="absmiddle" alt="mastodon" name="mastodon" onmouseover="mastodon.src='<?php bloginfo('template_directory'); ?>/images/social/mastodon.svg';" onMouseOut="mastodon.src='<?php bloginfo('template_directory'); ?>/images/social/mastodon_grey.svg';" /></a>
+        <a rel="me" href="https://mastodon.social/@spipau"></a> <!-- Link to Paul's profile on Mastodon -->
     </div>
 <!--/social -->
 
